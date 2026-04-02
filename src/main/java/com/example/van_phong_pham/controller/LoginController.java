60fb768be262bfcae34c1697b172015a189bfdeb
Lương <leducluong.08052005@gmail.com>
2026-04-02 17:23:29 +0700

    Implement basic auth/checkout MVC flow and Thymeleaf views

--- src/main/java/com/example/van_phong_pham/controller/LoginController.java
+++ src/main/java/com/example/van_phong_pham/controller/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CartController {
+public class LoginController {
 
-    @GetMapping("/cart")
-    public String cartPage() {
-        return "gio-hang";
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
     }
 }
